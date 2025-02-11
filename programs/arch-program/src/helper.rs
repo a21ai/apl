19bc4a0d365137ec19a30efc367b286651489b11
utxo detective <harrison@beckerich.org>
2025-02-11 13:39:08 -0600

    ensure passing

--- programs/arch-program/src/helper.rs
+++ programs/arch-program/src/helper.rs
@@ -50,24 +50,30 @@ pub fn get_state_transition_tx(accounts: &[AccountInfo]) -> Transaction {
 
 pub fn add_state_transition(transaction: &mut Transaction, account: &AccountInfo) {
     assert!(account.is_writable);
-    transaction.input.push(TxIn {
+
+    // Create input on heap to avoid large stack allocations
+    let txid = Txid::from_str(&hex::encode(account.utxo.txid())).unwrap();
+    let input = Box::new(TxIn {
         previous_output: OutPoint {
-            txid: Txid::from_str(&hex::encode(account.utxo.txid())).unwrap(),
+            txid,
             vout: account.utxo.vout(),
         },
         script_sig: ScriptBuf::new(),
         sequence: Sequence::MAX,
         witness: Witness::new(),
     });
+    transaction.input.push(*input);
 
     msg!("account utxo : {:?}", hex::encode(account.utxo.txid()));
 
-    let tx: Transaction = bitcoin::consensus::deserialize(
-        &get_bitcoin_tx(account.utxo.txid().try_into().unwrap()).unwrap(),
-    )
-    .unwrap();
-    transaction.output.push(TxOut {
+    // Get bitcoin tx data and create output on heap
+    let tx_data = get_bitcoin_tx(account.utxo.txid().try_into().unwrap()).unwrap();
+    let tx: Transaction = bitcoin::consensus::deserialize(&tx_data).unwrap();
+
+    let script_pubkey = ScriptBuf::from_bytes(get_account_script_pubkey(account.key).to_vec());
+    let output = Box::new(TxOut {
         value: tx.output[account.utxo.vout() as usize].value,
-        script_pubkey: ScriptBuf::from_bytes(get_account_script_pubkey(account.key).to_vec()),
+        script_pubkey,
     });
+    transaction.output.push(*output);
 }
